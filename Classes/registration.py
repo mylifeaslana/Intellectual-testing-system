5c96038dccae3df7bb58c405f126129f009233b1
DariaAlium <daria.kurguzikova@intel.com>
2020-03-05 19:59:37 +0300

    немножечко написано

--- Classes/registration.py
+++ Classes/registration.py
@@ -1,8 +1,39 @@
-def logIn(login,password):
-    a=False
+
+def check_login(login):
+    
+    if login in users:
+        return False
+    return True
+    
+def check_password(password):
+
+    if password < 8:
+        return False
+    return True
+
+def registration():
+    
+    login = input()
+    while !check_login(login):
+        print("Such name already exists \n Please, enter another")
+        login = input()
+        
+    print("Your password should be larger than 8")
+    password = input()
+    while !check_password(password):
+        print("Try again")
+        password = input()
+    
+    
+    
+
+def logIn(login, password): # вход
+    a = False
     if(a):
         raise Exception("что не так")
-    user="" #объект
+    template_user = "" #
     return user
-def tmpLogIn(login):
+
+
+def tmpLogIn(login): #временный вход
     pass
