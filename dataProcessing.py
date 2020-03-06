3b1e706197b6e57ae0a8ef1f90048d49ebc048f8
JuliaZimina <60311865+JuliaZimina@users.noreply.github.com>
2020-03-06 16:37:11 +0300

    Файл с пользователями

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,2 +1,3 @@
-users={'вася пупкин': {'password':'хана монтана'}}
+#login;password;status;ban;name,surname;father_name;date_of_birth;group;secret_question;secret_answer;email;tel
+users={}
 questions={}
