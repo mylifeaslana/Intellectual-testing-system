fd4ba02a43001e0dc8b9bc5f0a962ba5e547590a
JuliaZimina <60311865+JuliaZimina@users.noreply.github.com>
2020-03-06 16:48:16 +0300

    Шаблон строки в тестах

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,3 +1,5 @@
 #login;password;status;ban;name,surname;father_name;date_of_birth;group;secret_question;secret_answer;email;tel
 users={}
-questions={}
+#группа;вопрос;время;правильный ответ;ответ;ответ
+tests={}
+groups_of_questions=[]
